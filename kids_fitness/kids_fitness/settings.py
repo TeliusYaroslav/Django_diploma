7908b32c8d421351b99f46aad9f56e2cd2a44e35
Yaroslav <fil.i.al4ik@gmail.com>
2024-06-23 21:27:43 +0200

    start adaptive
    Co-authored-by: PanasenkoDiana <PanasenkoDiana@users.noreply.github.com>
    Co-authored-by: Ivan Kratenko <ruopodfg@users.noreply.github.com>
    Co-authored-by: Илья Булкин <IliaBulkin@users.noreply.github.com>
    Co-authored-by: Yaroslav Proidysvit <geniyhub@users.noreply.github.com>

--- kids_fitness/kids_fitness/settings.py
+++ kids_fitness/kids_fitness/settings.py
@@ -137,3 +137,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kidsfitness340@gmail.com'
+EMAIL_HOST_PASSWORD = 'xmxu gsth ozlj fmjg'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
